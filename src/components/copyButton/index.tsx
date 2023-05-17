3ed06af6e71e69d17b02ca7f1984ab9fd495b296
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-05-17 10:59:27 -0400

    Adding useToast to copy text

--- src/components/copyButton/index.tsx
+++ src/components/copyButton/index.tsx
@@ -2,7 +2,7 @@ import React, { FC, useState } from "react";
 
 import { Clipboard, CheckSquare } from "react-feather";
 
-import { Box, Center } from "@chakra-ui/react";
+import { Box, Center, HStack, Text, useToast } from "@chakra-ui/react";
 
 interface CopyButtonProps {
   stringToCopy: string;
@@ -10,14 +10,30 @@ interface CopyButtonProps {
 
 export const CopyButton: FC<CopyButtonProps> = ({ stringToCopy }) => {
   const [isCopied, setIsCopied] = useState(false);
+  const toast = useToast();
 
-  function handleClick() {
-    navigator.clipboard.writeText(stringToCopy);
+  async function handleClick() {
+    await navigator.clipboard.writeText(stringToCopy);
     setIsCopied(true);
 
     setTimeout(() => {
       setIsCopied(false);
     }, 1500);
+
+    toast({
+      title: "Copied to clipboard",
+      position: "top-right",
+      render(props) {
+        return (
+          <HStack p="4" bgColor="brightgreen.500">
+            <CheckSquare size="1.25rem" />
+            <Text>
+              <strong>{props.title}</strong>
+            </Text>
+          </HStack>
+        );
+      },
+    });
   }
 
   return (
